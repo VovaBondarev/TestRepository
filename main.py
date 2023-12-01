83efb5d8cebd4ab0b5afbcfdad236726dd13eec6
VovaBondarev <vova05072009@gmail.com>
2023-12-01 15:50:38 +0300

    решение задачи Спирограф

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-a = []
-with open("output.txt", "r") as file:
-    for line in file.readlines():
-        temp = int(line.replace("\n", ""))
-        a.append(temp)
-print(a)
+import turtle
+
+a = turtle.Turtle()
+color = input()
+r1 = int(input())
+r2 = int(input())
+n = int(input())
+p = 3.1416
+a.color(color)
+for i in range(n):
+    a.circle(r2)
+    a.lt(360 / n)
+    a.fd(2 * p * r1 / n)
+a.hideturtle()
+turtle.done()
+
+# magenta
+# 20
+# 150
+# 36
