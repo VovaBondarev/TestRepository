f4cb2064c8f233be010779fa8470b9007244449b
VovaBondarev <vova05072009@gmail.com>
2023-12-01 15:44:39 +0300

    решение задачи Рукава Архимеда

--- scratch_1.py
+++ scratch_1.py
@@ -1,21 +1,21 @@
 import turtle
 from math import cos, radians, sin
 
-archimed = turtle.Turtle()
+spiral_archimeda = turtle.Turtle()
 c = input()
 d = int(input())
 a = int(input())
 b = int(input())
-archimed.color(c)
+spiral_archimeda.color(c)
 for phi in range(0, 360, 360 // d):
     for teta in range(0, 360, 10):
         r = a + b * radians(teta)
         x = r * cos(radians(teta + phi))
         y = r * sin(radians(teta + phi))
-        archimed.pu()
-        archimed.goto(x, y)
-        archimed.pd()
-        archimed.dot()
+        spiral_archimeda.pu()
+        spiral_archimeda.goto(x, y)
+        spiral_archimeda.pd()
+        spiral_archimeda.dot()
 turtle.done()
 
 # silver
